01e44cb29e0e2af5d79ac912d84758591aad26bf
Priscila Padilla Latance <priscila.latance@gmail.com>
2022-03-17 17:26:08 -0300

    TesteGFT

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ordem de Números e Números Invertidos. Adicione valores acima de Zero:");
+            int numero = 15485448;
+            char[] numeroOrdem = numero.ToString().ToCharArray();
+            Array.Reverse(numeroOrdem);
+            string numeroInvertido;
+            Console.WriteLine($"{numero} --> {numeroInvertido = new string(numeroOrdem)}");
+
+
+        }
+    }
+}
