0c089358f06df3982386265ee4fd0785a5cbda42
Priscila Padilla Latance <priscila.latance@gmail.com>
2022-03-17 17:39:38 -0300

    TesteGFT

--- Exercicio2/Program.cs
+++ Exercicio2/Program.cs
@@ -6,18 +6,25 @@ namespace Exercicio2
     {
         static void Main(string[] args)
         {
-            string[] inteiros = Console.ReadLine().Split(" ");;
-            int total = 0, n=0, maior=0;
+            string numero = Console.ReadLine();
 
-            for(int i = 0; i < inteiros.Length; i++){
-                n = Convert.ToInt16(inteiros[i]);
-                total += n;
-                if(n > maior){
-                    maior = n;
+            if(numero != ""){
+                string[] inteiros = numero.Split(" ");
+                int total = 0, n=0, maior=0;
+
+                for(int i = 0; i < inteiros.Length; i++){
+                    n = Convert.ToInt16(inteiros[i]);
+                    total += n;
+                    if(n > maior){
+                        maior = n;
+                    }
                 }
+                Console.WriteLine($"O maior número é {maior}");
+                Console.WriteLine($"A soma dos valores é {total}");
             }
-            Console.WriteLine($"O maior número é {maior}");
-            Console.WriteLine($"A soma dos valores é {total}");
+            else{
+                    Console.WriteLine($"Digite um valor valido.");                
+            }
         }
     }
 }
