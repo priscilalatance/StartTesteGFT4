da80902a840f57ef990fe5cc42275a8440b013b2
Priscila Padilla Latance <priscila.latance@gmail.com>
2022-03-17 16:14:02 -0300

    Teste GFT

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inteiros = Console.ReadLine().Split(" ");;
+            int total = 0, n=0, maior=0;
+
+            for(int i = 0; i < inteiros.Length; i++){
+                n = Convert.ToInt16(inteiros[i]);
+                total += n;
+                if(n > maior){
+                    maior = n;
+                }
+            }
+            Console.WriteLine($"O maior número é {maior}");
+            Console.WriteLine($"A soma dos valores é {total}");
+        }
+    }
+}
